b03103979614fdbfff09c3e2fa2b16589e2e5e3b
Mattias Schlenker <mattias.schlenker@checkmk.com>
2024-07-02 12:07:47 +0200

    move examples to v2 API

--- examples/devel_check_plugins_snmp/flintstone_setup_check.py
+++ examples/devel_check_plugins_snmp/flintstone_setup_check.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 # This file is explained in the Checkmk User Guide:
 # https://docs.checkmk.com/master/en/devel_check_plugins_snmp.html#parse_function
+#
+# Store in your Checkmk site at:
+# local/lib/python3/cmk_addons/plugins/flintstone_setup_check/agent_based/flintstone_setup_check.py
 
-from .agent_based_api.v1 import register, Result, Service, startswith, SNMPTree, State
+from .agent_based_api.v2 import SNMPSection, CheckPlugin, Result, Service, startswith, SNMPTree, State
 
 def parse_flintstone(string_table):
     # print(string_table)
@@ -27,7 +30,7 @@ def check_flintstone(section):
     else:
         yield Result(state=State.OK, summary="All required information is available.")
 
-register.snmp_section(
+snmp_section_flintstone_setup = SNMPSection(
     name = "flintstone_base_config",
     parse_function = parse_flintstone,
     detect = startswith(
@@ -40,9 +43,9 @@ register.snmp_section(
     ),
 )
 
-register.check_plugin(
+check_plugin__flintstone_setup = CheckPlugin(
     name = "flintstone_setup_check",
-    sections = ["flintstone_base_config"],
+    sections = [ "flintstone_base_config" ],
     service_name = "Flintstone setup check",
     discovery_function = discover_flintstone,
     check_function = check_flintstone,
--- examples/devel_check_plugins_snmp/flintstone_setup_check_bare_minimum.py
+++ examples/devel_check_plugins_snmp/flintstone_setup_check_bare_minimum.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 # This file is explained in the Checkmk User Guide:
 # https://docs.checkmk.com/master/en/devel_check_plugins_snmp.html#scaffold
+#
+# Store in your Checkmk site at:
+# local/lib/python3/cmk_addons/plugins/flintstone_setup_check/agent_based/flintstone_setup_check.py
 
-from .agent_based_api.v1 import register, Result, Service, startswith, SNMPTree, State
+from .agent_based_api.v2 import SNMPSection, CheckPlugin, Result, Service, startswith, SNMPTree, State
 
 def parse_flintstone(string_table):
     return {}
@@ -13,16 +16,16 @@ def discover_flintstone(section):
 def check_flintstone(section):
     yield Result(state=State.OK, summary="Everything is fine")
 
-register.snmp_section(
+snmp_section_flintstone_setup = SNMPSection(
     name = "flintstone_base_config",
     parse_function = parse_flintstone,
     detect = startswith(".1.3.6.1.2.1.1.1.0", "Flintstone"),
     fetch = SNMPTree(base='.1.3.6.1.2.1.1', oids=['4.0']),
 )
 
-register.check_plugin(
+check_plugin__flintstone_setup = CheckPlugin(
     name = "flintstone_setup_check",
-    sections = ["flintstone_base_config"],
+    sections = [ "flintstone_base_config" ],
     service_name = "Flintstone setup check",
     discovery_function = discover_flintstone,
     check_function = check_flintstone,
