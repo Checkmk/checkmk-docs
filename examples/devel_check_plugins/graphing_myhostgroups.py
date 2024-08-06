2c3ac54d248da21a2077e7dce5ce64cfe7f80fa7
Thomas Koslowski <thomas.koslowski@checkmk.com>
2024-08-06 16:21:23 +0200

    KNW-1256 - ready for review

--- examples/devel_check_plugins/graphing_myhostgroups.py
+++ examples/devel_check_plugins/graphing_myhostgroups.py
@@ -1,17 +1,46 @@
 #!/usr/bin/env python3
-# This file is NOT yet explained in the Checkmk User Guide, it will be soon:
-# https://docs.checkmk.com/master/en/devel_check_plugins.html
+# This file is explained in the Checkmk User Guide:
+# https://docs.checkmk.com/master/en/devel_check_plugins.html#metrics_advanced
 # 
 # Store in your Checkmk site at:
-# local/lib/python3/cmk_addons/plugins/myhostgroups/graphing/myhostgroups.py
+# ~/local/lib/python3/cmk_addons/plugins/myhostgroups/graphing/graphing_myhostgroups.py
 #
-# Do not to forget to delete the old files using the unversioned API:
-# local/share/check_mk/web/plugins/metrics/myhostgroups_advanced_metrics.py etc.
+# If you have implemented the example presented in Checkmk 2.2.0 for the unversioned API, delete the files:
+# ~/local/share/check_mk/web/plugins/metrics/myhostgroups_advanced_metrics.py.
+# ~/local/share/check_mk/web/plugins/perfometer/myhostgroups_advanced_perfometer.py
 
 from cmk.graphing.v1 import Title
 from cmk.graphing.v1.graphs import Graph, MinimalRange
 from cmk.graphing.v1.metrics import Color, DecimalNotation, Metric, Unit
 from cmk.graphing.v1.perfometers import Closed, FocusRange, Open, Perfometer
+
+metric_myhostgroups_services = Metric(
+    name = "num_services",
+    title = Title("Number of services in group"),
+    unit = Unit(DecimalNotation("")),
+    color = Color.PINK,
+)
+
+metric_myhostgroups_services_ok = Metric(
+    name = "num_services_ok",
+    title = Title("Number of services in OK state"),
+    unit = Unit(DecimalNotation("")),
+    color = Color.BLUE,
+)
+
+metric_myhostgroups_hosts = Metric(
+    name = "num_hosts",
+    title = Title("Number of hosts in group"),
+    unit = Unit(DecimalNotation("")),
+    color = Color.PINK,
+)
+
+metric_myhostgroups_hosts_up = Metric(
+    name = "num_hosts_up",
+    title = Title("Number of hosts in UP state"),
+    unit = Unit(DecimalNotation("")),
+    color = Color.YELLOW,
+)
 
 metric_myhostgroups_services_ok_perc = Metric(
     name = "services_ok_perc",
@@ -27,33 +56,6 @@ metric_myhostgroups_hosts_up_perc = Metric(
     color = Color.PURPLE,
 )
 
-metric_myhostgroups_services_ok = Metric(
-    name = "num_services_ok",
-    title = Title("Number of services in OK state"),
-    unit = Unit(DecimalNotation("")),
-    color = Color.BLUE,
-)
-
-metric_myhostgroups_hosts_up = Metric(
-    name = "num_hosts_up",
-    title = Title("Number of hosts in UP state"),
-    unit = Unit(DecimalNotation("")),
-    color = Color.YELLOW,
-)
-
-metric_myhostgroups_services = Metric(
-    name = "num_services",
-    title = Title("Number of services in group"),
-    unit = Unit(DecimalNotation("")),
-    color = Color.PINK,
-)
-
-metric_myhostgroups_hosts = Metric(
-    name = "num_hosts",
-    title = Title("Number of hosts in group"),
-    unit = Unit(DecimalNotation("")),
-    color = Color.PINK,
-)
 
 graph_myhostgroups_combined = Graph(
     name = "services_ok_comparison",
@@ -61,6 +63,7 @@ graph_myhostgroups_combined = Graph(
     simple_lines=[ "num_services", "num_services_ok" ],
     minimal_range=MinimalRange(0, 50),
 )
+
 
 perfometer_myhostgroups_advanced = Perfometer(
     name = "myhostgroups_advanced",
