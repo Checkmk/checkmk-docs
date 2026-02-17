5e2673dae25dda6ef71a9f80ac0006c2c0a5d202
Mattias Schlenker <mattias.schlenker@checkmk.com>
2026-02-17 14:32:53 +0100

    KNW-2104 pick-24 less empty lines

--- testing/remove_all_translations_from_tbx.rb
+++ testing/remove_all_translations_from_tbx.rb
@@ -6,16 +6,43 @@ require "nokogiri"
 indata = File.read(ARGV[0])
 inxml = Nokogiri::XML.parse(indata)
 
+outdata = '<!DOCTYPE martif PUBLIC "ISO 12200:1999A//DTD MARTIF core (DXFcdV04)//EN" "TBXcdv04.dtd">
+<martif type="TBX" xml:lang="en">
+    <martifHeader>
+        <fileDesc>
+            <sourceDesc>
+                <p>Translate Toolkit</p>
+            </sourceDesc>
+        </fileDesc>
+    </martifHeader>
+    <text>
+        <body>
+            
+        </body>
+    </text>
+</martif>'
+outxml = Nokogiri::XML.parse(outdata)
+
 rcount = 0
 
-inxml.search(".//body/termEntry/langSet[@xml:lang='en_US']/tig/term").each { |n| 
-    if n.inner_text.size > 0
-        puts "Removing: " + n.content
-        n.content = ""
+outxml.search(".//body/termEntry").each { |n|
+    n.remove
+}
+body = outxml.search(".//body")[0]
+
+inxml.search(".//body/termEntry").each { |n|
+    m = n.search("./langSet[@xml:lang='en_US']/tig/term")[0]
+    if m.inner_text.size > 0
+        puts "Removing: " + m.content
+        m.content = ""
         rcount += 1
+        body.add_child(n.clone)
     end
 }
 
 puts "Removed #{rcount} strings."
 
-File.open(ARGV[1], "w") { |f| f.write(inxml) }
+File.open(ARGV[1], "w") { |f|
+    f.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+    f.write(outxml.to_html)
+}
