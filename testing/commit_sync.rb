c07a6250e367b36785a8da50375124c0ffbf305e
Mattias Schlenker <mattias.schlenker@checkmk.com>
2026-05-05 14:50:14 +0200

    pick-24 clean up, add possibility to force filenames or ticket IDs

--- testing/commit_sync.rb
+++ testing/commit_sync.rb
@@ -53,6 +53,18 @@ def check_against_ignores(commitinfo)
 	return false
 end
 
+def check_against_forced(commitinfo)
+	commitwords = commitinfo[2].gsub(',', ' ').split
+	commitwords.each { |c|
+		return true if @cfg["force_tickets"].include? c
+	}
+	commitinfo[3].each { |f|
+		fname = f.split("/")[-1]
+		return true if @cfg["force_files"].include? fname
+	}
+	return false
+end
+
 def get_config()
 	@cfg = {
 		"ignore_files" => [],
@@ -65,19 +77,75 @@ def get_config()
 		@cfg['cfgfile'] = cfgfile
 	}
 	opts.on('--since', :REQUIRED) { |i| @cfg["start_date"] = i }
+	opts.on('--only-files', :REQUIRED) { |i| @cfg["only_files"] = i.split(',') }
+	opts.on('--force-tickets', :REQUIRED) { |i| @cfg["force_tickets"] = i.split(',') }
 	opts.parse!
+	@cfg["only_files"] = [] unless @cfg.has_key? "only_files"
 end
 
+def try_to_pick(commitinfo)
+	ret = system("git cherry-pick #{commitinfo[1]}")
+	unless ret
+		puts "+++> Pick failed. Abort the commit and continue loop or exit? [E/a] "
+		secdec = gets
+		if secdec.strip == "" || secdec.strip =~ /^e/
+			exit 1
+		else
+			commitinfo[3].each { |f| @files_with_skipped_commits.push f }
+			unless system("git cherry-pick --abort")
+				exit 1
+			end
+		end
+	end
+end
+
+def ask_and_pick(missingcommits)
+	missingcommits.reverse.each { |c|
+		@allfiles.push c[3]
+		puts "#{c[0]} + #{c[1]} + #{c[2]}"
+		commitwords = c[2].gsub(',', ' ').split
+		nskipped = 0
+		c[3].each { |f|
+			s = ""
+			if @files_with_skipped_commits.include? f
+				s = " <= has skipped commits!"
+				nskipped += 1
+			end
+			puts "    #{f}#{s}"
+		}
+		ask = true
+		decision = ""
+		# Forced tickets/files first:
+		if check_against_forced(c)
+			defdec = "y"
+			puts "===> Try to pick? [Y/n] "
+		elsif !check_against_ignores(c)
+			puts "===> No decision needed, ticket or files on ignore list."
+			c[3].each { |f| @files_with_skipped_commits.push f }
+			ask = false
+		elsif nskipped > 0
+			defdec = "n"
+			puts "===> Try to pick? [N/y] "
+		elsif commitwords.include? @cfg["keyword"]
+			defdec = "y"
+			puts "===> Try to pick? [Y/n] "
+		else
+			defdec = "n"
+			puts "===> Try to pick? [N/y] "
+		end
+		decision = gets if ask == true
+		decision = defdec if decision.strip == ""
+		if decision.strip =~ /^y/i
+			try_to_pick(c)
+		else
+			c[3].each { |f| @files_with_skipped_commits.push f }
+		end
+	}
+end
+
+
 @cfg = {}
-
-# Force these tickets to behave like pick-24
-# Add a comment to each ticket to make cleaning easier later
-# @force_tickets = [
-# 	"KNW-9999", # Example: Mattias is working on this...
-# ]
-
 get_config
-print @cfg
 
 missingcommits = []
 @allfiles = []
@@ -93,56 +161,8 @@ clist.each { |c|
     missingcommits.push c unless check_present(olist, c[0])
 }
 
-puts missingcommits 
+ask_and_pick(missingcommits)
 
-missingcommits.reverse.each { |c|
-	@allfiles.push c[3]
-	puts "#{c[0]} + #{c[1]} + #{c[2]}"
-	commitwords = c[2].gsub(',', ' ').split
-	nskipped = 0
-	c[3].each { |f|
-		s = ""
-		if @files_with_skipped_commits.include? f
-			s = " <= has skipped commits!"
-			nskipped += 1
-		end
-		puts "    #{f}#{s}"
-	}
-	if check_against_ignores(c)
-		if nskipped > 0
-			defdec = "n"
-			puts "===> Try to pick? [N/y] "
-		elsif commitwords.include? @cfg["keyword"]
-			defdec = "y"
-			puts "===> Try to pick? [Y/n] "
-		else
-			defdec = "n"
-			puts "===> Try to pick? [N/y] "
-		end
-		decision = gets
-		decision = defdec if decision.strip == ""
-		if decision.strip =~ /^y/i
-			ret = system("git cherry-pick #{c[1]}")
-			unless ret
-				puts "+++> Pick failed. Abort the commit and continue loop or exit? [E/a] "
-				secdec = gets
-				if secdec.strip == "" || secdec.strip =~ /^e/
-					exit 1
-				else
-					c[3].each { |f| @files_with_skipped_commits.push f }
-					unless system("git cherry-pick --abort")
-						exit 1
-					end
-				end
-			end
-		else
-			c[3].each { |f| @files_with_skipped_commits.push f }
-		end
-	else
-		puts "===> No decision needed, ticket or files on ignore list."
-		c[3].each { |f| @files_with_skipped_commits.push f }
-	end
-}
 
 #~ @allfiles = @allfiles.sort.uniq
 
