6efe81559ec1309d09bb602f1f3241f6b54ce0ed
Mattias Schlenker <mattias.schlenker@checkmk.com>
2026-08-07 11:09:18 +0200

    allow lower case ticket numbers pick-25

--- testing/commit_sync.rb
+++ testing/commit_sync.rb
@@ -127,7 +127,7 @@ def check_against_ignores(commitinfo)
 	return true if @cfg["ignore_commits"].include?(commitinfo[1])
 	commitwords = commitinfo[2].gsub(',', ' ').split
 	commitwords.each { |c|
-		return true if @cfg["ignore_tickets"].include? c
+		return true if @cfg["ignore_tickets"].include? c.upcase
 	}
 	commitinfo[3].each { |f|
 		fname = f.split("/")[-1]
